6cac6bc61ce1bdd46314aef856ab937a6d0739a4
Luiz Felipe Pereira de lima <luisfelipepereira508@gmail.com>
2026-03-15 01:05:18 -0300

    Updat

--- db.js
+++ db.js
@@ -7,8 +7,27 @@ dotenv.config()
 dns.setDefaultResultOrder('ipv4first')
 
 const connectionString = process.env.DATABASE_URL
+
+if (!connectionString) {
+    throw new Error('DATABASE_URL nao definida.')
+}
+
+const url = new URL(connectionString)
+const hostname = url.hostname
+
+let resolvedHost = hostname
+try {
+    const lookup = await dns.promises.lookup(hostname, { family: 4 })
+    resolvedHost = lookup.address
+} catch (error) {
+    console.warn('Falha ao resolver IPv4, usando hostname original.', error)
+}
+
 const sql = postgres(connectionString, {
-    ssl: 'require'
+    host: resolvedHost,
+    ssl: {
+        servername: hostname
+    }
 })
 
 export default sql
