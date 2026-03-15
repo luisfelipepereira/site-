1a979bf2c31d64afac70aa03171917b814aa65ad
Luiz Felipe Pereira de lima <luisfelipepereira508@gmail.com>
2026-03-15 00:43:54 -0300

    ..ponto

--- db.js
+++ db.js
@@ -1,7 +1,10 @@
+import dns from 'node:dns'
 import dotenv from 'dotenv'
 import postgres from 'postgres'
 
 dotenv.config()
+
+dns.setDefaultResultOrder('ipv4first')
 
 const connectionString = process.env.DATABASE_URL
 const sql = postgres(connectionString, {
